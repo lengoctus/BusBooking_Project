f8876f0727cea325963abd4ca069ffb64a15561d
hongson <hongson16420@gmail.com>
2020-05-29 22:13:58 +0700

    giao diện trang home cho khách hàng

---
+++ BusBooking_Project/ViewComponents/BookingPartialForm.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusBooking_Project.ViewComponents
+{
+    [ViewComponent(Name = "BookingPartialForm")]
+    public class BookingPartialForm : ViewComponent
+    {       
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("index");
+        }
+    }
+}
