38d5ec543a4a64fb920c09d64903a0780f158f98
kienca <kienca1199@gmail.com>
2020-06-01 14:17:09 +0700

    Schedule view and controllers

---
+++ BusBooking_Project/Controllers/ScheduleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusBooking_Project.Controllers
+{
+    [Route("schedule")]
+    public class ScheduleController : Controller
+    {
+        [Route("~/")]
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+
+        [Route("")]
+        [Route("schedule_Detail")]
+        public IActionResult Schedule_Detail()
+        {
+            return View("Schedule_Detail");
+        }
+    }
+}
