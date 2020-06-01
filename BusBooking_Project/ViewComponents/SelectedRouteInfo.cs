587ca55a0ac162aebebde5fa63c102458d0e3932
hongson <hongson16420@gmail.com>
2020-06-01 23:38:25 +0700

    Giao diện: chọn chuyến, chọn ghế, thông tin khách hàng, thanh toán

---
+++ BusBooking_Project/ViewComponents/SelectedRouteInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusBooking_Project.ViewComponents
+{
+    [ViewComponent(Name = "SelectedRouteInfo")]
+    public class SelectedRouteInfo : ViewComponent
+    {       
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("index");
+        }
+    }
+}
---
+++ BusBooking_Project/ViewComponents/SelectedSeatInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusBooking_Project.ViewComponents
+{
+    [ViewComponent(Name = "SelectedSeatInfo")]
+    public class SelectedSeatInfo : ViewComponent
+    {       
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("index");
+        }
+    }
+}
