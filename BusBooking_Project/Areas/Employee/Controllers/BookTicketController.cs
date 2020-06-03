887904d0f9f52fa3598721297aa4fb392a6e3f72
kienca <kienca1199@gmail.com>
2020-06-03 17:06:11 +0700

    Employee View (Task of Kien)

---
+++ BusBooking_Project/Areas/Employee/Controllers/BookTicketController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusBooking_Project.Areas.Employee.Controllers
+{
+    [Area("employee")]
+    [Route("employee/bookticket")]
+    public class BookTicketController : Controller  
+    {
+       
+       
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        
+       
+        [Route("login")]
+        public IActionResult Login()    
+        {
+            return View("Login");
+        }
+
+      
+        [Route("repw")]
+        public IActionResult Repassword()   
+        {
+            return View("Repw");
+        }
+    }
+}
