254352411344f8696eb0afacd116d1c4e0fee8e0
hongson <hongson16420@gmail.com>
2020-06-08 12:48:04 +0700

    Sửa layout

--- BusBooking_Project/Repository/CsRepository/SeatRepo.cs
+++ BusBooking_Project/Repository/CsRepository/SeatRepo.cs
@@ -14,25 +14,9 @@ namespace BusBooking_Project.Repository.CsRepository
         public SeatRepo(ConnectDbContext db) : base(db)
         {
         }
+        
 
-        public Task<IQueryable<Seat>> GetByBusId(int idBus)
-        {
-            try
-            {
-                var listSeat = GetAll().Result.Where(p => p.BusId == idBus);
-                if (listSeat.Count() > 0)
-                {
-                    return Task.FromResult(listSeat);
-                }
-                return Task.FromResult<IQueryable<Seat>>(null);
-            }
-            catch (Exception e)
-            {
-                var error = e.InnerException.Message;
-                return Task.FromResult<IQueryable<Seat>>(null);
-            }
-        }
-
+        
         public override bool CheckIsExists(Seat entity)
         {
             try
