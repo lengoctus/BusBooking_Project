e22e2fb0a7dc2ecdd54a5adb9f6d1e5edb1351a2
Ngọc Tú <giabang205@gmail.com>
2020-06-21 11:59:31 +0700

    data Cancel Booking

--- BusBooking_Project/Models/ModelsView/BookingView.cs
+++ BusBooking_Project/Models/ModelsView/BookingView.cs
@@ -25,7 +25,10 @@ namespace BusBooking_Project.Models.ModelsView
         public string StationNameTo { get; set; }
         public decimal CategoryPrice { get; set; }
         public decimal RoutePrice { get; set; }
-
+        public string BusCode { get; set; }
+        public string SeatCode { get; set; }
+        public string ClientName { get; set; }
+        public string ClientDescription { get; set; }
 
     }
 }
